81dc3c586b37fec011539e3ee11ffd6c8bf11fa2
icltemp <35255128+icltemp@users.noreply.github.com>
2018-01-25 13:52:10 +0530

    image res 1024
    updated images with 1024 resulution

--- research/object_detection/object_detection_tutorial.py
+++ research/object_detection/object_detection_tutorial.py
@@ -108,13 +108,13 @@ with detection_graph.as_default():
 # In[ ]:
 
 
-#label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-#categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-#category_index = label_map_util.create_category_index(categories)
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
 
-    with open(PATH_TO_LABELS, 'r') as fin:
+   # with open(PATH_TO_LABELS, 'r') as fin:
 
-        category_index = [line.rstrip('\n') for line in fin]        
+     #   category_index = [line.rstrip('\n') for line in fin]        
 
 # ## Helper code
 
--- research/object_detection/objectdetection_tut_webcam.py
+++ research/object_detection/objectdetection_tut_webcam.py
@@ -61,17 +61,17 @@ from utils import visualization_utils as vis_util
 
 
 # What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+MODEL_NAME = 'icube_test_graph'
+#MODEL_FILE = MODEL_NAME + '.tar.gz'
+#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join('data', 'icube-detection.pbtxt')
 
-NUM_CLASSES = 90
+NUM_CLASSES = 4
 
 
 # ## Download Model
@@ -79,13 +79,13 @@ NUM_CLASSES = 90
 # In[ ]:
 
 
-opener = urllib.request.URLopener()
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
-  file_name = os.path.basename(file.name)
-  if 'frozen_inference_graph.pb' in file_name:
-    tar_file.extract(file, os.getcwd())
+#opener = urllib.request.URLopener()
+#opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+#tar_file = tarfile.open(MODEL_FILE)
+#for file in tar_file.getmembers():
+ # file_name = os.path.basename(file.name)
+ # if 'frozen_inference_graph.pb' in file_name:
+  #  tar_file.extract(file, os.getcwd())
 
 
 # ## Load a (frozen) Tensorflow model into memory.
